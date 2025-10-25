8663c00a51c42223558a37361c7dc94ecc8899b5
Gkhundadze <giorgi.khundadze15b@geolab.edu.ge>
2025-10-25 16:11:48 +0400

    create all required endpoints

--- backend/server.js
+++ backend/server.js
@@ -5,6 +5,11 @@ import path from 'path';
 import { v4 as uuidv4 } from 'uuid';
 import dotenv from 'dotenv';
 import chalk from 'chalk';
+import dayjs from 'dayjs';
+import 'dayjs/locale/ka.js';
+import joi from 'joi';
+
+dayjs.locale('ka');
 
 dotenv.config();
 
@@ -28,6 +33,102 @@ app.get('/api/todos', (req, res) => {
         res.status(200).json(todos);
     })
 })
+
+// --------------Add New Todo---------------
+app.post('/api/todos', (req, res) => {
+    fs.readFile(dataFilePath, 'utf8', async (err, data) => {
+        if (err) {
+            console.log(chalk.red('Failed to read data'));
+            return res.status(500).json({ error: 'Failed to read data' });
+        }
+
+        const date = dayjs()
+
+        const todos = JSON.parse(data)
+
+        const schema = joi.object({
+            title: joi.string().min(3).max(30).required()
+        })
+
+        const { error } = schema.validate({ title: req.body.title })
+        if (error) {
+            return res.status(400).json({ message: error.details[0].message })
+        }
+
+        const newTodo = {
+            id: uuidv4(),
+            title: req.body.title,
+            completed: false,
+            date: date.format('DD.MM.YYYY, HH:mm')
+        }
+
+        todos.push(newTodo)
+        await fs.writeFile(dataFilePath, JSON.stringify(todos, null, 2), (err) => {
+            if (err) {
+                console.log(chalk.red('Failed to write data'));
+                return res.status(500).json({ error: 'Failed to write data' });
+            }
+        })
+        console.log(chalk.green('New Todo Successfully Added'));
+        res.status(201).json(newTodo);
+        
+    })
+})
+
+app.delete('/api/todos/:id', (req, res) => {
+    fs.readFile(dataFilePath, 'utf8', async (err, data) => {
+        if (err) {
+            console.log(chalk.red('Failed to read data'));
+            return res.status(500).json({ error: 'Failed to read data' });
+        }           
+
+        const todos = JSON.parse(data);
+        const todoIndex = todos.findIndex(todo => todo.id === req.params.id);
+
+        if (todoIndex === -1) {
+            return res.status(404).json({ error: 'Todo not found' });
+        }
+
+        todos.splice(todoIndex, 1);
+
+        await fs.writeFile(dataFilePath, JSON.stringify(todos, null, 2), (err) => {
+            if (err) {
+                console.log(chalk.red('Failed to write data'));
+                return res.status(500).json({ error: 'Failed to write data' });
+            }
+        });
+
+        console.log(chalk.green('Todo Successfully Deleted'));
+        res.status(204).send();
+    });
+});
+
+app.put('/api/todos/:id/toggle', (req, res) => {
+    fs.readFile(dataFilePath, 'utf8', async (err, data) => {
+        if (err) {
+            console.log(chalk.red('Failed to read data'));
+            return res.status(500).json({ error: 'Failed to read data' });
+        }
+        const todos = JSON.parse(data);
+        const todo = todos.find(todo => todo.id === req.params.id);
+
+        if (!todo) {
+            return res.status(404).json({ error: 'Todo not found' });
+        }
+
+        todo.completed = !todo.completed;
+
+        await fs.writeFile(dataFilePath, JSON.stringify(todos, null, 2), (err) => {
+            if (err) {
+                console.log(chalk.red('Failed to write data'));
+                return res.status(500).json({ error: 'Failed to write data' });
+            }
+        });
+
+        console.log(chalk.green('Todo Successfully Toggled'));
+        res.status(200).json(todo);
+    });
+});
 
 // --------------Run Server---------------
 app.listen(PORT, () => {
